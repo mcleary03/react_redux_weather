39e4315e283f25db5f8ebbf0fd2f145a4ea2fad6
Michael Cleary <mcleary03@gmail.com>
2018-11-26 16:01:27 -0800

    Updates

---
+++ precache-manifest.23e136fc98e673de79275e7c1269eabc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aac9a5f3ff13f0b9727b",
+    "url": "/react_redux_weather/static/js/runtime~main.aac9a5f3.js"
+  },
+  {
+    "revision": "bb8552a805eef64317d7",
+    "url": "/react_redux_weather/static/js/main.bb8552a8.chunk.js"
+  },
+  {
+    "revision": "4b0d61d7c792d805afef",
+    "url": "/react_redux_weather/static/js/1.4b0d61d7.chunk.js"
+  },
+  {
+    "revision": "bb8552a805eef64317d7",
+    "url": "/react_redux_weather/static/css/main.2ccbec24.chunk.css"
+  },
+  {
+    "revision": "84c4922f23b634ea6385cf2c19ad59c2",
+    "url": "/react_redux_weather/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/react_redux_weather/precache-manifest.23e136fc98e673de79275e7c1269eabc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react_redux_weather/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
